340df4870c20c34091617703deb6cd41ca132a89
ipiga <i.pigazzini@campus.unimib.it>
2017-06-15 10:01:15 +0200

    Run autowrong on trump and apple

--- tools/testAutowrong.js
+++ tools/testAutowrong.js
@@ -2,16 +2,16 @@
 const autowrong = require('autowrong');
 const options = {adjacent: 0.05, double: 0.00, order: 0.00}
 var fs = require("fs");
-//var text = fs.readFileSync("../dataset/trump_tweets.txt").toString('utf-8');
-var text = fs.readFileSync("../dataset/apple_tweets.txt").toString('utf-8');
+var text = fs.readFileSync("../dataset/trump_tweets.txt").toString('utf-8');
+//var text = fs.readFileSync("../dataset/apple_tweets_cleaned.txt").toString('utf-8');
 var textByLine = text.split("\r\n");
 console.log(textByLine)
 var maxIndex = textByLine.length
 console.log(maxIndex)
-for(i = 0; i < maxIndex; i++){
-	var perturb = textByLine[i].split("\t")
-	console.log(perturb[0])
-	console.log(perturb[1])
-	//fs.appendFileSync("../dataset/trump_tweets_autowrong.txt", perturb[0] + "\t" + autowrong(perturb[1], options) + "\r\n")
-	fs.appendFileSync("../dataset/apple_tweets_autowrong.txt", perturb[0] + "\t" + autowrong(perturb[1], options) + "\r\n")
+for(i = 0; i < maxIndex - 1; i++){
+    var perturb = textByLine[i].split("\t")
+    console.log(perturb[0])
+    console.log(perturb[1])
+    fs.appendFileSync("../dataset/trump_tweets_autowrong.txt", perturb[0] + "\t" + autowrong(perturb[1], options) + "\r\n")
+    //fs.appendFileSync("../dataset/apple_tweets_autowrong.txt", perturb[0] + "\t" + autowrong(perturb[1], options) + "\r\n")
 }
